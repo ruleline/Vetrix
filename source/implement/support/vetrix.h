ce9dbea59dcaca35200cf5b691617ec963fac5e1
ruleline <168268798+ruleline@users.noreply.github.com>
2025-07-10 00:47:04 +0800

    feat(project): introduce shared header file with core definitions (#5)
    - feat(project): introduce shared header file with core definitions

---
+++ source/implement/support/vetrix.h
@@ -0,0 +1,27 @@
+/**
+ * @file vetrix.h
+ * @brief Vetrix Project.
+ * @author ruleline (ruleline@outlook.com)
+ * @since 2025-07-10
+ *
+ * @authors ruleline (ruleline@outlook.com)
+ * @date 2025-07-10
+ * @version 0.00.001
+ *
+ * @copyright ©2025 ruleline
+ *
+ * @details
+ * -----------------------------------------------------------------------------
+ *    version   |    date    |     author     |             comments
+ * ------------ | ---------- | -------------- | --------------------------------
+ *   0.00.001   | 2025-07-10 |    ruleline    | initial commit
+ * -----------------------------------------------------------------------------
+ */
+
+#if !defined VETRIX_H
+#define VETRIX_H
+
+#include "vetrix.config.h"
+#include "vlibc.h"
+
+#endif /* !defined VETRIX_H */
